a586e63031697bf2983c007b4f3c23788fbdafa0
santhosh <santoshkrishna63@gmail.com>
2023-10-23 10:15:01 +0530

    Initial Commit

---
+++ Liven_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Liven_Task
+//
+//  Created by Santhosh Konduru on 23/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
